f6a73387e2d75635a6a402a4e785dca936663803
TanyaPetkova <taniapetkova@abv.bg>
2015-04-10 16:02:54 +0300

    README file added

--- Programming/C# I/01.Introduction-To-Programming/02.HelloCSharp/HelloCSharp.cs
+++ Programming/C# I/01.Introduction-To-Programming/02.HelloCSharp/HelloCSharp.cs
@@ -2,6 +2,11 @@
 {
     using System;
 
+    /// <summary>
+    /// Create, compile and run a “Hello C#” console application.
+    /// Ensure you have named the application well (e.g. “”HelloCSharp”).
+    /// You should submit the Visual Studio project holding the HelloCSharp class as part of your homework.
+    /// </summary>
     public class HelloCSharp
     {
         public const string Greeting = "Hello C#";
